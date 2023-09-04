2e59f9352872fccee9fe97054d80704c36899c89
lucapedr01 <p2001luca@gmail.com>
2023-09-04 18:23:26 +0200

    feat: example - move with arrow

--- examples/game.rs
+++ examples/game.rs
@@ -5,24 +5,20 @@ fn main() {
     let mut x_position = 200.0;
     let mut y_position = 30.0;
 
-    let mut x_direction = 1.0;
-    let mut y_direction = 1.0;
-
-    let speed = 1.0;
-
     set_event_handler(move |context, event| match event {
         Event::Draw => {
-            x_position += x_direction * speed;
-            y_position += y_direction * speed;
-            if x_position <= 0.0 || x_position >= 500.0 {
-                x_direction *= -1.0;
-            }
-            if y_position <= 0.0 || y_position >= 500.0 {
-                y_direction *= -1.0;
-            }
             context.clear_screen_to_color(0.0, 0.0, 0.3, 1.0);
             context.draw_rectangle(x_position, y_position, 100., 100., 1.0, 0.0, 0.0, 1.0);
         }
-        _ => {}
+        Event::KeyDown(key) => {
+            let move_amount = 20.0;
+            match key {
+                Key::Left => x_position -= move_amount,
+                Key::Right => x_position += move_amount,
+                Key::Up => y_position += move_amount,
+                Key::Down => y_position -= move_amount,
+                Key::Space => {}
+            }
+        }
     })
 }
